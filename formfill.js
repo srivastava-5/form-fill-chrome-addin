390649f78ebbea64fb704b28d095d77fc8e751a5
ishwar.rimal@gmail.com <ishwar.rimal@gmail.com>
2020-05-17 14:41:52 +0530

    structure changes

--- formfill.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(gotMessage);
-
-function gotMessage(message, sender, sendresponse) {
-        console.log(message.flow);
-        console.log(message.page)
-        var loanAmount = document.getElementById("mat-input-0");
-        loanAmount.value = "300000";
- 
-      };
